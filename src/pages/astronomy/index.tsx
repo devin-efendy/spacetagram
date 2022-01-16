72031191eb5401055dffdc479cc281ce3a4208fa
devin-efendy <devinefendy.dev@gmail.com>
2022-01-15 21:26:17 -0600

    Improved page error handling

--- src/pages/astronomy/index.tsx
+++ src/pages/astronomy/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
 import { ApodData } from "apod-types";
 import { format, subDays } from "date-fns";
+import { Flex } from "@chakra-ui/react";
 
 import { Astronomy, NavBar } from "../../components";
 import config from "../../config";
@@ -14,6 +15,26 @@ interface Props {
 }
 
 const AstronomyPage: NextPage<Props> = ({ apodData }) => {
+  let renderPageContent;
+
+  if (apodData && apodData.length > 0) {
+    renderPageContent = <Astronomy apodData={apodData} />;
+  } else {
+    renderPageContent = (
+      <Flex
+        h="500px"
+        p="4"
+        d="flex"
+        align="center"
+        justify="center"
+        fontSize="20"
+        fontWeight="semibold"
+      >
+        Sorry, there is an error and we are trying our best to fix it! :(
+      </Flex>
+    );
+  }
+
   return (
     <>
       <NavBar />
@@ -26,14 +47,14 @@ const AstronomyPage: NextPage<Props> = ({ apodData }) => {
         <meta name="keywords" content="astronomy, nasa, pictures, space" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Astronomy apodData={apodData} />
+      {renderPageContent}
     </>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   let nasaRes;
-  let apodData;
+  let apodData = [];
 
   const todayDate = new Date();
   const lastWeekDate = subDays(todayDate, 7);
@@ -45,8 +66,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     nasaRes = await axios.get(
       `${NASA_APOD_API_URL}?api_key=${config.NASA_API_KEY}&start_date=${formattedLastWeekDate}&end_date=${formattedTodayDate}`
     );
-  } catch (error) {
-    console.error("Failed to retrieve pictures from NASA API...");
+  } catch (error: any) {
+    console.error(
+      "Failed to retrieve pictures from NASA API --",
+      error.response.data
+    );
   }
 
   const userCookies = ctx.req.cookies?.user
